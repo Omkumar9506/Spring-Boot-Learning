01356b1f9e0699bbff8d324b150fb4e31034cc42
Omkumar9506 <hari.9506563662@gmail.com>
2026-09-19 16:41:40 +0530

    initial commit

---
+++ Lecture02/demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("home")
+    public String hello(){
+        return "<h1>Welcome to my home page</h1>";
+    }
+
+    @GetMapping("login")
+    public String login(){
+        return "<h1>Welcone to Login page</h1>";
+    }
+
+    @GetMapping("course")
+    public String course(){
+        return "<h1>Welcome to our Course page" +
+                "<h1>Purchase our Spring boot course</h1>";
+    }
+
+    @GetMapping("signup")
+    public String signUp(){
+        return "<h1>SignUp now and access this web application</h1>";
+    }
+
+    @GetMapping("about")
+    public String about(){
+        return "<h1>Welcome to our about page</h2>";
+    }
+
+}
